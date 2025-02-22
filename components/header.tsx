ddcc180bd03eaf3c6be2131a6873c12fd3689398
johnTeller <liemthanhbui2000@gmail.com>
2025-02-22 14:13:41 +0700

    add clerk

---
+++ components/header.tsx
@@ -0,0 +1,67 @@
+import {
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="flex gap-3 p-3 bg-red-700 items-center justify-between">
+      <div className="text-white"> Logo </div>
+      <Link href="/menu" className="text-white flex gap-1">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="lucide lucide-cup-soda"
+        >
+          <path d="m6 8 1.75 12.28a2 2 0 0 0 2 1.72h4.54a2 2 0 0 0 2-1.72L18 8" />
+          <path d="M5 8h14" />
+          <path d="M7 15a6.47 6.47 0 0 1 5 0 6.47 6.47 0 0 0 5 0" />
+          <path d="m12 8 1-6h2" />
+        </svg>{" "}
+        Menu{" "}
+      </Link>
+      <Link href="/bill" className="text-white flex gap-1">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="lucide lucide-receipt"
+        >
+          <path d="M4 2v20l2-1 2 1 2-1 2 1 2-1 2 1 2-1 2 1V2l-2 1-2-1-2 1-2-1-2 1-2-1-2 1Z" />
+          <path d="M16 8h-6a2 2 0 1 0 0 4h4a2 2 0 1 1 0 4H8" />
+          <path d="M12 17.5v-11" />
+        </svg>
+        Bill
+      </Link>
+
+      <div className="flex gap-3">
+        <SignedOut>
+          <div className=" p-3 border border-gray-200 rounded-lg">
+            <SignInButton> Đăng Nhập </SignInButton>
+          </div>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </header>
+  );
+}
